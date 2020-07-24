dc0bd65257868b856ddb655f5cdc74d22315556c
platoscave <platoscave60@gmail.com>
2020-07-24 15:47:20 +0200

    fix html

--- htmldemo/htmldemo.js
+++ htmldemo/htmldemo.js
@@ -24,18 +24,19 @@ export default class HTMLDemo extends Scene {
         // init will setup html styles and retreive fonts asynchronously
         await super.init()
 
-        const testDoc = await this.importHtml("./test.html")
+        const testStr = await this.importHtml("./test.html")
 
-        let labelObj3d1 = new HtmlObject3D(testDoc.body, this.fonts, this.darkPageProps)
+        let labelObj3d1 = new HtmlObject3D(testStr, this.fonts, this.darkPageProps)
         labelObj3d1.rotateY = Math.PI / 6;
         labelObj3d1.updateMatrix();
-        //labelObj3d1.position.set (-1000, 0, 0 )
+        labelObj3d1.position.set (-1000, 0, 0 )
         this.scene.add(labelObj3d1);
         labelObj3d1.key = 'darkPage'
         this.selectableMeshArr.push(labelObj3d1.getBackgroundMesh())
 
-/* 
-        let labelObj3d2 = new HtmlObject3D(testDoc.body, this.fonts, this.lightPageProps)
+        const wikiStr = await this.importHtml("https://en.wikipedia.org/api/rest_v1/page/summary/Three.js")
+        var wikiObj = JSON.parse(wikiStr)
+        let labelObj3d2 = new HtmlObject3D(wikiObj.extract_html, this.fonts, this.lightPageProps)
         labelObj3d2.rotateY = -Math.PI / 6;
         labelObj3d2.updateMatrix();
         labelObj3d2.position.set ( 1000, 0, 0 )
@@ -44,26 +45,23 @@ export default class HTMLDemo extends Scene {
         this.selectableMeshArr.push(labelObj3d2.getBackgroundMesh())
  
 
-        let parser = new DOMParser();
 
-        let txt3 = `<h3>Simple Label</h3><p>This callout points to a simple label.</p>`
-        let calloutDoc = parser.parseFromString(txt3, 'text/html');
-        let labelObj3d3 = new HtmlObject3D(calloutDoc.body, this.fonts, this.calloutProps)
-        //labelObj3d3.position.set ( 0, 100, 0 )
+        let txt3 = `<b>Simple Label</b><t>This callout points to a simple label.</t>`
+        let labelObj3d3 = new HtmlObject3D(txt3, this.fonts, this.calloutProps)
+        labelObj3d3.translateY(20)
         //labelObj3d3.rotateY = -Math.PI / 6;
         this.scene.add(labelObj3d3);
         labelObj3d3.key = 'callout'
-        this.selectableMeshArr.push(labelObj3d3.getBackgroundMesh())
+        this.selectableMeshArr.push(labelObj3d3.getBackgroundMesh()) 
 
 
-        let txt4 = `<p><b>Simple Label that spans Multiple Lines</b></p>`
-        let labelDoc = parser.parseFromString(txt4, 'text/html');
-        let labelObj3d4 = new HtmlObject3D(labelDoc.body, this.fonts, this.labelProps)
+        let txt4 = `<b>Simple Label that spans Multiple Lines. <br>Simple Label that spans Multiple Lines</b>`
+        let labelObj3d4 = new HtmlObject3D(txt4, this.fonts, this.labelProps)
         //labelObj3d3.position.set ( 1000, 0, 0 )
         //labelObj3d3.rotateY = -Math.PI / 6;
         this.scene.add(labelObj3d4);
         labelObj3d4.key = 'label'
-        this.selectableMeshArr.push(labelObj3d4.getBackgroundMesh()) */
+        this.selectableMeshArr.push(labelObj3d4.getBackgroundMesh())
 
 
     }
